d095c676df0f3fe4dd9b58c950901a8956e531a5
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-07-04 17:58:39 +0100

    add more option

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -4,7 +4,7 @@ import wifi from '../assets/icons/wifi.png'
 import lamp from '../assets/icons/lamp.png'
 import energy from '../assets/icons/energy.png'
 
-function DropDown({ text }) {
+function DropDown() {
 
     const [open, setOpen] = useState(false);
 
@@ -26,14 +26,14 @@ function DropDown({ text }) {
                 onMouseLeave={handleMouseLeave}
             // onClick={() => setOpen((event) => !event)}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="blue" class="w-3 h-3">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                 </svg>
             </button>
             {open && (
-                <div className='bg-white border w-[145px] h-[78px] rounded p-1 absolute bottom-full'>
-                    <p className='text-xs break-words'>
-                        {text}
+                <div className='bg-white border rounded p-1 absolute bottom-full'>
+                    <p className='text-xs break-words text-blue-900 font-semibold'>
+                        Modifier
                     </p>
                 </div>
             )}
@@ -43,24 +43,26 @@ function DropDown({ text }) {
 
 function SubForm() {
 
-    const [openProfile, setOpenProfile] = useState(false);
+    const [openSubForm, setOpenSubForm] = useState(false);
 
     return (
         <div className='flex items-center absolute'>
             <div>
                 <button
-                    onClick={() => setOpenProfile((event) => !event)}                >
+                    onClick={() => setOpenSubForm((event) => !event)}                >
                     <img className='w-4 h-4 rounded-full' src={energy} alt="" />
                 </button>
             </div>
-            {/* {openProfile && ( */}
+            {openSubForm && (
                 <div className=' mt-56 rounded bg-black bg-opacity-20 w-80'>
                     <div className='text-sm py-4 px-2 cursor-pointer rounded flex flex-col gap-4'>
                         <div className='flex justify-between items-center'>
                             <p className='font-bold text-sm'>Genset</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
+                            <button onClick={() => setOpenSubForm((event) => !event)} >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </button>
                         </div>
                         <div className='flex flex-col gap-4 p-6 bg-white rounded-md'>
                             <div className='flex flex-col items-center'>
@@ -73,7 +75,7 @@ function SubForm() {
                                     <div className='flex text-xs gap-1'>
                                         <p className='text-green-500 font-semibold'>OUI</p>
                                         <div className='relative'>
-                                            <DropDown text={'When the countdown is finished, the system will automatically move to the next question.'} />
+                                            <DropDown />
                                         </div>
                                     </div>
                                 </div>
@@ -82,7 +84,7 @@ function SubForm() {
                                     <div className='flex text-xs gap-1'>
                                         <p className='text-red-500 font-semibold'>NON</p>
                                         <div className='relative'>
-                                            <DropDown text={'When the countdown is finished, the system will automatically move to the next question.'} />
+                                            <DropDown />
                                         </div>
                                     </div>
                                 </div>
@@ -90,7 +92,7 @@ function SubForm() {
                         </div>
                     </div>
                 </div>
-            {/* )} */}
+            )}
         </div>
     );
 };
